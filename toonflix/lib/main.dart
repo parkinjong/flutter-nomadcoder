fece21e2c386f9101612b7a1ff3465d5398a1994
InJongPark <parkinjong@gmail.com>
2023-07-23 22:38:29 +0900

    add scarffold

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -9,7 +9,9 @@ class App extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello world!'),
+      home: Scaffold(
+        body: Text('Hello world!'),
+      ),
     );
   }
 }
